d4eee2f5214a9430e2ea9a2a19a1dd3b9ea3dc53
Aviscad <anthonyviscad@gmail.com>
2022-08-22 20:36:57 -0600

    Adding UI

--- js/script.js
+++ js/script.js
@@ -1,4 +1,74 @@
-const options = ["Rock", "Paper", "Scissors"];
+let rock = document.querySelector("#rock"),
+  paper = document.querySelector("#paper"),
+  scissors = document.querySelector("#scissors"),
+  selections = document.querySelector("#selection"),
+  score = document.querySelector("#score"),
+  selectImage = "",
+  playerWins = 0,
+  computerWins = 0,
+  resultado = document.querySelector("#resultado"),
+  times = 5;
+
+rock.addEventListener("click", () => {
+  playerVsComputer("rock");
+});
+
+paper.addEventListener("click", () => {
+  playerVsComputer("paper");
+});
+
+scissors.addEventListener("click", () => {
+  playerVsComputer("scissors");
+});
+
+function playerVsComputer(playerSelection) {
+  let computerSelection = getRandomOption();
+  selectImage = computerSelection;
+  let result = "";
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    result = "Rock beats Scissors, you win!";
+    playerWins++;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    result = "Paper beats Rocks, you lose!";
+    computerWins++;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    result = "Scissors beats Paper, you lose!";
+    computerWins++;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    result = "Paper beats Rocks, you win!";
+    playerWins++;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    result = "Scissors beats Paper, you win!";
+    playerWins++;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    result = "Rock beats Scissors, you lose!";
+    computerWins++;
+  } else if (playerSelection == computerSelection) {
+    result = "Its a Tie";
+  }
+  resultado.textContent = `${result}`;
+  selections.innerHTML = "";
+  score.innerHTML = "";
+  let img = document.createElement("img"),
+    imgPlayer = document.createElement("img");
+
+  imgPlayer.src = "./assets/img/" + playerSelection + ".png";
+  img.src = "./assets/img/" + selectImage + ".png";
+
+  if (playerWins == 5 || computerWins == 5) {
+    score.innerHTML = `
+    <p>The Game is Over!</p>
+    <p>Final Score</p>
+    <p>Player - ${playerWins}:${computerWins} - Computer</p>
+    `;
+    playerWins = computerWins = 0;
+  } else if (playerWins < 5 || computerWins < 5) {
+    score.textContent = `Player - ${playerWins}:${computerWins} - Computer`;
+  }
+
+  selections.appendChild(imgPlayer);
+  selections.appendChild(img);
+}
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -6,65 +76,7 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function playGame() {
-  let playerWins = 0;
-  let computerWins = 0;
-  let times = prompt("How many times do you want to play?");
-  let result = "";
-
-  for (let i = 1; i <= times; i++) {
-    let computer = getRandomOption();
-    let playerSelection = prompt(
-      "Enter your selection (rock, paper, scissors)"
-    );
-    console.log(`P: ${playerSelection} | C: ${computer}`);
-    if (playerSelection.toLowerCase() == "rock" && computer == "scissors") {
-      result = "Player 1 Wins: Rock beats Scissors";
-      playerWins++;
-    } else if (playerSelection.toLowerCase() == "rock" && computer == "paper") {
-      result = "Computer Wins: Paper beats Rocks";
-      computerWins++;
-    } else if (
-      playerSelection.toLowerCase() == "paper" &&
-      computer == "scissors"
-    ) {
-      result = "Computer Wins: Scissors betas Paper";
-      computerWins++;
-    } else if (playerSelection.toLowerCase() == "paper" && computer == "rock") {
-      result = "Player 1 Wins: Paper beats Rocks";
-      playerWins++;
-    } else if (
-      playerSelection.toLowerCase() == "scissors" &&
-      computer == "paper"
-    ) {
-      result = "Player 1 Wins: Scissors betas Paper";
-      playerWins++;
-    } else if (
-      playerSelection.toLowerCase() == "scissors" &&
-      computer == "rock"
-    ) {
-      result = "Computer 1 Wins: Rock beats Scissors";
-      computerWins++;
-    } else if (playerSelection.toLocaleLowerCase() == computer) {
-      result = "Its a Tie";
-    }
-
-    console.log(result);
-  }
-
-  if (playerWins > computerWins) {
-    console.log("Player Wins " + playerWins + ":" + computerWins);
-  } else if (computerWins > playerWins) {
-    console.log("Computer Wins " + computerWins + ":" + playerWins);
-  } else {
-    console.log(
-      "Its a Tie Player" + playerWins + ":" + computerWins + "Computer"
-    );
-  }
-}
-
 function getRandomOption() {
+  const options = ["Rock", "Paper", "Scissors"];
   return options[getRandomIntInclusive(0, options.length - 1)].toLowerCase();
 }
-
-playGame();
